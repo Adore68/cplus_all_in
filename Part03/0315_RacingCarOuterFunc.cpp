e023621fba1e4db190dde51173f093fa26b730c2
Jihyun Kim <adore68@naver.com>
2019-02-24 14:57:09 +0900

    24 Feb studying updated

--- Part03/0315_RacingCarOuterFunc.cpp
+++ Part03/0315_RacingCarOuterFunc.cpp
@@ -1,3 +1,5 @@
+/* puting function declaration inside structure, function definition outside */
+
 #include <iostream>
 using namespace std;
 
@@ -10,45 +12,50 @@ namespace CAR_CONST
 		FUEL_STEP	= 2,
 		ACC_STEP	=10,
 		BRK_STEP	=10
-	};  // !
+	}; 
 }
+
 struct Car
 {
 	char gamerID[CAR_CONST::ID_LEN];
 	int fuelGauge;
 	int curSpeed;
 
-	void ShowCarState()
-	{
-		cout<<"gamer ID: "<<gamerID<<endl;
-		cout<<"fuel gauge: "<<fuelGauge<<"%"<<endl;
-		cout<<"current speed: "<<curSpeed<<"km/s"<<endl<<endl;
-	}
-	void Accel()
-	{
-		if(fuelGauge<=0)
-			return;
-		else
-			fuelGauge-=CAR_CONST::FUEL_STEP;
-
-		if((curSpeed+CAR_CONST::ACC_STEP)>=CAR_CONST::MAX_SPD)
-		{
-			curSpeed=CAR_CONST::MAX_SPD;
-			return;
-		}
-
-		curSpeed+=CAR_CONST::ACC_STEP;
-	}
-	void Break()
-	{
-		if(curSpeed<CAR_CONST::BRK_STEP)
-		{
-			curSpeed=0;
-			return;
-		}
-		curSpeed-=CAR_CONST::BRK_STEP;
-	}
+	void ShowCarState();
+	void Accel();
+	void Break();
 };
+
+void Car::ShowCarState()
+{
+	cout<<"gamer ID: "<<gamerID<<endl;
+	cout<<"fuel gauge: "<<fuelGauge<<endl;
+	cout<<"current speed: "<<curSpeed<<"km/s"<<endl<<endl;
+}
+void Car::Accel()
+{
+	if(fuelGauge<=0)
+		return;
+	else
+		fuelGauge-=CAR_CONST::FUEL_STEP;
+
+	if((curSpeed+CAR_CONST::ACC_STEP)>=CAR_CONST::MAX_SPD)
+	{
+		curSpeed=CAR_CONST::MAX_SPD;
+		return;
+	}
+
+	curSpeed+=CAR_CONST::ACC_STEP;
+}
+void Car::Break()
+{		
+	if(curSpeed<CAR_CONST::BRK_STEP)
+	{
+		curSpeed=0;
+		return;
+	}
+	curSpeed-=CAR_CONST::BRK_STEP;
+}
 
 
 int main(void)
